afe963c79530db9b28ab4eda5ef4cfe51d15dc1b
Bandari Naveen <bandarin29@gmail.com>
2024-10-21 15:24:01 +0530

    table color changed

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -329,7 +329,7 @@ const Dashboard = () => {
             </Paper>
           </Grid>
         </Grid>
-        <table className="min-w-full bg-white mb-10">
+        <table className="min-w-full bg-blue-100 mb-10">
           <thead>
             <tr>
               <th className="py-2">ID</th>
