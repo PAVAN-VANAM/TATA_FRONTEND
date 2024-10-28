3d2f0835c253aa6131e44b70e63eeeff393315fd
Pavan Vanam <105368989+PAVAN-VANAM@users.noreply.github.com>
2024-10-28 11:41:55 +0530

    REview

--- src/components/attendance.jsx
+++ src/components/attendance.jsx
@@ -17,7 +17,7 @@ const Attendance = () => {
 
   const fetchToken = useCallback(async () => {
     try {
-      const response = await axios.put(`${import.meta.env.VITE_API}/batch/generate`, {
+      const response = await axios.post(`${import.meta.env.VITE_API}/batch/generate`, {
         batch_name: batchName,
       });
 
